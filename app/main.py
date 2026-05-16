4466770d34b84c66e995d1d7acf0a44974710cdb
Agung Haryawan <agungharyawan58@gmail.com>
2026-05-17 00:02:22 +0900

    perbaikan main.py

--- app/main.py
+++ app/main.py
@@ -2,14 +2,19 @@
 
 from fastapi import FastAPI
 from app.database import create_user_table
-from app.middleware import LogingMiddleware
+from app.middleware import LoggingMiddleware
 from app.routers.auth_router import router as auth_router
 
-create_user_table()
 
 app = FastAPI()
 
-app.add_middleware(LogingMiddleware)
+
+@app.on_event("startup")
+def startup():
+    create_user_table()
+
+
+app.add_middleware(LoggingMiddleware)
 app.include_router(auth_router)
 
 
