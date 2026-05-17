6c960a13675a0cf15ef97465a0e2a69a8969cd6c
Agung Haryawan <agungharyawan58@gmail.com>
2026-05-17 12:30:13 +0900

    fix database url

--- app/database.py
+++ app/database.py
@@ -19,10 +19,15 @@ import os
 load_dotenv()
 
 
-DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    "postgresql://postgres:KLtNHJrxaitSDZvMlzyLyGHgczCIseEF@postgres.railway.internal:5432/railway",
-)
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL is not set")
+
+# Fix Railway postgres://
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
 
 engine = create_engine(DATABASE_URL)
 
