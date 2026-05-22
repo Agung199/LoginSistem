817e2aadc5e59fda998b3fb3d0971f2e09367381
Agung Haryawan <agungharyawan58@gmail.com>
2026-05-22 21:00:05 +0900

    add railway start command

---
+++ alembic/env.py
@@ -0,0 +1,67 @@
+from logging.config import fileConfig
+import os
+
+from alembic import context
+from dotenv import load_dotenv
+from sqlalchemy import engine_from_config, pool
+
+from app.db.base import Base
+from app.models.user_model import User  # noqa: F401
+
+load_dotenv()
+
+config = context.config
+
+DATABASE_PUBLIC_URL = os.getenv("DATABASE_PUBLIC_URL")
+
+if not DATABASE_PUBLIC_URL:
+    raise ValueError("DATABASE_PUBLIC_URL not found in .env")
+
+config.set_main_option(
+    "sqlalchemy.url",
+    DATABASE_PUBLIC_URL,
+)
+
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
+
+target_metadata = Base.metadata
+
+
+def run_migrations_offline() -> None:
+    url = config.get_main_option("sqlalchemy.url")
+
+    context.configure(
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def run_migrations_online() -> None:
+    connectable = engine_from_config(
+        config.get_section(config.config_ini_section, {}),
+        prefix="sqlalchemy.",
+        poolclass=pool.NullPool,
+    )
+
+    with connectable.connect() as connection:
+        context.configure(
+            connection=connection,
+            target_metadata=target_metadata,
+        )
+
+        with context.begin_transaction():
+            context.run_migrations()
+
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
+
+print("DATABASE_PUBLIC_URL =", DATABASE_PUBLIC_URL)
--- app/models/user_model.py
+++ app/models/user_model.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, Integer, String, TIMESTAMP, text
-from app.database import Base
+from app.db.base import Base
 
 
 class User(Base):
